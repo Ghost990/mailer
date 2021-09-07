e4e5c437efd7cd50c2e6d08ffc5f92cc80ad6473
Zsolt <zsolt.nagy@vodafone.com>
2021-09-07 14:56:02 +0200

    init

---
+++ app.js
@@ -0,0 +1,37 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const nodemailer = require("nodemailer")
+
+const app = express()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const contactAddress = "hey@yourwebsite.com"
+
+const mailer = nodemailer.createTransport({
+  host: "mail.allusionn.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "hello@allusionn.com", // generated ethereal user
+      pass: "Noonelives177", // generated ethereal password
+    },
+})
+
+app.post("/contact", function (req, res) {
+  mailer.sendMail(
+    {
+      from: req.body.from,
+      to: [contactAddress],
+      subject: req.body.subject || "[No subject]",
+      html: req.body.message || "[No message]",
+    },
+    function (err, info) {
+      if (err) return res.status(500).send(err)
+      res.json({ success: true })
+    }
+  )
+})
+
+app.listen(3000)
